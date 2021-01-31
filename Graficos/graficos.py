a3df14295f17e710c7adb378730b57d63b5e9bec
sgloayza <sgloayza@espol.edu.ec>
2021-01-30 19:01:23 -0500

    graficos steam

--- Graficos/graficos.py
+++ Graficos/graficos.py
@@ -9,45 +9,72 @@ import warnings; warnings.filterwarnings(action='once')
 # STEAM
 # Correlación entre variables calificacion Metacritic y precio
 def scatterplot1():
+
+    # lectura
     df = pd.read_csv("juegosSteam.csv")
+
+    # condiciones
     df = df[df.metacritic.notnull()]
     df = df[df["precio"]>0]
     df = df[df["metacritic"] > 0]
+
+    # estilo
     sns.set_style("ticks")
-    gridobj = sns.lmplot(x="metacritic", y="precio", data=df, height=5, aspect=11/5)
-    gridobj.set(xlim=(55, 95), ylim=(0, 65))
-    plt.title("Regresión lineal simple")
+
+    # variables y limites
+    gridobj = sns.lmplot(x="metacritic", y="precio", data=df, height=5, aspect=10/5)
+    gridobj.set(xlim=(55,95), ylim=(0,65))
+
+    # presenta
+    plt.title("Regresión lineal simple", fontsize=16, color='red')
     plt.tight_layout()
     plt.show()
-#scatterplot1()
+scatterplot1()
 # Ranking de géneros
 def barplot1():
 
-    # Crear método para la lectura
+    # lectura
     df = pd.read_csv("etiquetas.csv")
+
+    # ordeno
     df = df.sort_values(by=['nroDeJuegos'], ascending=False)
 
+    # escojo los 10 primeros
     generos = df["nombreE"].values[0:10]
     njuegos = df["nroDeJuegos"].values[0:10]
 
     fig, ax = plt.subplots()
-    fig.set_size_inches(14,5) # tamano ventana
-    width = 0.4 # grosor de barras
-    ind = np.arange(len(njuegos))  # ubicacion x
-    ax.barh(ind, njuegos, width, color="blue")
+
+    # tamano ventana
+    fig.set_size_inches(14,5)
+
+    # grosor de barras
+    width = 0.5
+
+    # ubicacion x
+    ind = np.arange(len(njuegos))
+
+    # estilos y limites
+    color = ["#0032FF", "#1D48FF", "#2D55FF", "#4A6CFF", "#607EFF", "#7690FF", "#889FFF", "#9EB0FF", "#AFBEFF", "#C4CFFF"]
+    for i in ind:
+        ax.barh(i,njuegos[i], width, color=color[i])
     ax.set_yticks(ind + width / 2)
     ax.set_yticklabels(generos, minor=False)
     ax.spines["top"].set_visible(False)
     ax.spines["left"].set_visible(False)
     ax.spines["right"].set_visible(False)
     ax.set_xlim([0,55000])
+
+
     # etiquetas sobre barras
     for i, v in enumerate(njuegos):
-        ax.text(v + 3, i + .25, str(v), color='blue', fontweight='bold')
-    plt.yticks(fontsize=8, rotation=45)
-    plt.title('Popularidad de los géneros en los juegos de video')
-    plt.xlabel('Número de Juegos')
-    plt.ylabel('Géneros')
+        ax.text(v + 3, i + .25, str(v), color='#032F83', fontweight='bold')
+    plt.yticks(fontsize=10, rotation=25, color='black') #juegos nombres
+    plt.title('Popularidad de los géneros en los juegos', fontsize=16, color='red')
+    plt.xlabel('Número de Juegos', fontsize=16, color='red')
+    plt.ylabel('Géneros', fontsize=16, color='red')
+
+    #presenta
     plt.show()
 #barplot1()
 
