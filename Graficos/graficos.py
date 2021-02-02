6b9254d2ac6458dc65e80d834a162d2eb8a51a76
gdmora <gdmora@espol.edu.ec>
2021-02-01 20:40:54 -0500

    Eneba csv added, plots added

--- Graficos/graficos.py
+++ Graficos/graficos.py
@@ -78,27 +78,113 @@ def barplot1():
 
     #presenta
     plt.show()
-#barplot1()
-
-
-
-
-
-# FANATICAL
-
-
-
-
-
 
 # ENEBA
 
+# Composición: Treemap del top 10 de publicadores de videojuegos
+def treemap():
+    # Leemos el csv.
+    df_raw = pd.read_csv("juegosEneba.csv")
+
+    # Agrupamos por publisher y contamos cuántos hay por clase.
+    df = df_raw.groupby('Publisher').size().reset_index(name='counts')
+    # Sort de los datos
+    df = df.sort_values(by=["counts"], ascending=False)[0:10]
+    # Agregamos las etiquetas
+    labels = df.apply(lambda x: str(x[0]) + "\n (" + str(x[1]) + ")", axis=1)
+    sizes = df['counts'].values.tolist()
+    # Decoración
+    colors = [plt.cm.Spectral(i/float(len(labels))) for i in range(len(labels))]
+
+    # Graficamos el treemap
+    plt.figure(figsize=(12, 8), dpi=80)
+    squarify.plot(sizes=sizes, label=labels, text_kwargs={'fontsize': 10}, color=colors, alpha=.8)
+
+    plt.title('Top 10 Publishers')
+    plt.axis('off')
+    plt.show()
 
 
+# Desviación: La variación de valores según género
+def dotplot():
+    df_raw = pd.read_csv("juegosEneba.csv")
+
+    # Filtro para evitar filas sin datos
+    df = df_raw[df_raw["Valoración del usuario"] != "0"]
+    df = df[df["Generos"].notnull()]
+
+    # Separamos en dos array las columnas que vamos a graficar
+    generos_raw = df["Generos"].values.tolist()
+    valoracionUsuario_raw = df["Valoración del usuario"].values.tolist()
+    valoracionUsuario = []
+
+    generos = []
+    valores = []
+
+    # Genera array con generos y valores
+    for valor in valoracionUsuario_raw:
+        arr = valor.split("/")
+        a = float(arr[0])
+        b = int(arr[1])
+        c = round(a/b * 100)
+        valoracionUsuario.append(c)
+
+    for i in range(len(generos_raw)):
+        arr = generos_raw[i].split("|")
+        for j in range(len(arr)):
+            generos.append(arr[j])
+            valores.append(valoracionUsuario[i])
+
+    # Crea un dataframe a partir de los array
+    data = {"Generos": generos, "ValoracionUsuario": valores}
+
+    df = pd.DataFrame(data, columns=["Generos", "ValoracionUsuario"])
+
+    # Saca el promedio
+    df["mean"] = df.groupby("Generos")["ValoracionUsuario"].transform("mean")
+
+    # Borra repetidos
+    df.drop_duplicates(subset="Generos", keep="first", inplace=True)
+
+    # Borra columna
+    del df["ValoracionUsuario"]
+
+    df = df.sort_values(by=["mean"], ascending=False)
+
+    x = df.loc[:, ['mean']]
+
+    # Prepara los valores
+    df['mean_z'] = (x-x.mean())/x.std()
+    df['colors'] = ['red' if x < 0 else 'darkgreen' for x in df['mean_z']]
+    df.sort_values('mean_z', inplace=True)
+    df.reset_index(inplace=True)
+
+    # Dibuja el gráfico
+    plt.figure(figsize=(12,8), dpi= 80)
+    plt.scatter(df.mean_z, df.index, s=450, alpha=.6, color=df.colors)
+    for x, y, tex in zip(df.mean_z, df.index, df.mean_z):
+        t = plt.text(x, y, round(tex, 1), horizontalalignment='center',
+                     verticalalignment='center', fontdict={'color':'white'})
+
+    # Decoración
+    # Bordes finos
+    plt.gca().spines["top"].set_alpha(.3)
+    plt.gca().spines["bottom"].set_alpha(.3)
+    plt.gca().spines["right"].set_alpha(.3)
+    plt.gca().spines["left"].set_alpha(.3)
+
+    plt.yticks(df.index, df.Generos)
+    plt.title('Dotplot: Divergencia en las valoraciones de usuario', fontdict={'size':20})
+    plt.xlabel('$Valoración de Usuario$')
+    plt.grid(linestyle='--', alpha=0.5)
+    plt.xlim(-2.5, 2.5)
+    plt.show()
 
 
+#dotplot()
+#treemap()
 
-
+# FANATICAL
 
 # Composición, disponibilidad en plataformas.
 def piechart():
